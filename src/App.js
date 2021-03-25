dffa40e8378cf82e5e76002d02c9af2a64143073
Krasimir <73705162+reveroJS@users.noreply.github.com>
2021-03-25 09:46:20 +0200

    add guest info page

--- src/App.js
+++ src/App.js
@@ -8,12 +8,14 @@ import Footer from "./components/Footer"
 import ContactUs from "./components/ContactUs";
 import Login from './components/Login';
 import Register from './components/Register/Register';
+import Home from './components/Home/Home';
 
 function App() {
   return (
     <div className="App">
       <Header />
       <Switch>
+        <Route path="/" exact component={Home} />
         <Route path="/contact" component={ContactUs} />
         <Route path="/Login" component={Login} />
         <Route path="/register" component={Register} />
---
+++ src/components/Home/Home.js
@@ -0,0 +1,15 @@
+import "./Home.css";
+
+const Home = () => {
+
+    return (
+    
+            <div id="guestHome">
+                <img className="box" src="/Human-Food-Your-Dogs-Can-And-Can-Not-Eat.webp" alt="" />
+            </div>
+    );
+
+
+}
+
+export default Home;
