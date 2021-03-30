55430e027ebd5e41a3ace1dc7be9c94ed9c8b45f
Krasimir <73705162+reveroJS@users.noreply.github.com>
2021-03-30 15:25:57 +0300

    adding data from firebase firestore

---
+++ src/components/Meals/MealCard/MealCard.js
@@ -0,0 +1,32 @@
+import "./MealCard.css";
+import { Link } from "react-router-dom";
+
+
+const MealCard = ({
+    id,
+    name,
+    description,
+    imageUrl
+}) => {
+
+    return (
+        <div className="card">
+            <div className="imgBox">
+                <img src={imageUrl} alt={name} />
+            </div>
+            <div className="content">
+                <div>
+                    <h2>{name}</h2>
+                    <p>{description}</p>
+                    <Link className="buyButton" to="/">See More</Link>
+                </div>
+            </div>
+        </div>
+    );
+
+
+
+}
+
+
+export default MealCard;
--- src/components/Meals/Meals.js
+++ src/components/Meals/Meals.js
@@ -1,59 +1,89 @@
 import "./Meals.css";
+import { db, auth } from "../../services/firebase";
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import * as productServices from "../../services/productServices";
+import MealCard from "./MealCard";
+
 
 const Meals = () => {
+
+    const [product, setProduct] = useState([]);
+
+    useEffect(() => {
+        db.collection('meals')
+            .get()
+            .then((res) => {
+                let meals = []
+                res.forEach((doc) => {
+                    let data = doc.data()
+                    meals.push(data)
+                })
+                console.log(meals)
+                setProduct(meals)
+            })
+    }, [])
 
 
     return (
         <section>
             <div className="conteiner-meals">
-                <div className="card">
+                <>
+                    {product.map((x) => <MealCard key={x.id} {...x} />)}
+                    {/* {console.log({product})}   */}
+                    {/* {product.forEach((x) => <MealCard key={x.id} {...x} />)} */}
+                </>
+                {/* <div className="card">
                     <div className="imgBox">
-                        <img src="/beef/beef_1-9813e0a4e1462922f2ebbbfc2de2b180.jpg" alt="beef" />
+                        <img src="/beef/beef1.jpg" alt="beef" />
                     </div>
                     <div className="content">
                         <div>
-                            <h2>Food One</h2>
-                            <p>Reprehenderit dolore laboris incididunt pariatur eu ex est do velit officia excepteur . Et sicing aliquip culpa eiusmod.</p>
-                            <input href="" type="button" value="Buy"/>
+                            <h2>Barkin' Beef</h2>   
+                            <p>Our high-protein, grain-free beef recipe beats the competition by leaps and bounds. Perfectly crafted by our chefs to be...grain-free beef recipe beats the competition by leaps and bounds.grain-free beef recipe beats the competition by leaps and bounds.</p>
+                            <Link className="buyButton" to="/">See More</Link>
                         </div>
                     </div>
-                </div>
-                <div className="card">
+                </div> */}
+                {/* <div className="card">
                     <div className="imgBox">
-                        <img src="/chicken/chicken_1-3ba5ab9a5d71e3c04343c589daf974b9.jpg" alt="chiken" />
+                        <img src="/chicken/chicken1.jpg" alt="chiken.png" />
                     </div>
                     <div className="content">
                         <div>
-                            <h2>Food Two</h2>
-                            <p>Reprehenderit dolore laboris incididunt pariatur eu ex est do velit officia excepteur . Et sicing aliquip culpa eiusmod.</p>
+                            <h2>Chompin Chicken</h2>
+                            <p>Finally, a high-protein, low-fat chicken dish fit for our furry friends! Grain-free and packed with whole food ingredients to...</p>
+                            <Link className="buyButton" to="/">See More</Link>
                         </div>
                     </div>
-                </div><div className="card">
+                </div> */}
+                {/* <div className="card">
                     <div className="imgBox">
-                        <img src="/lamb/lamb_1-c1620723d608ce17c141685ed6ef31d9.jpg" alt="beef" />
+                        <img src="/lamb/lamb1.jpg" alt="beef" />
                     </div>
                     <div className="content">
                         <div>
-                            <h2>Food Three</h2>
-                            <p>Reprehenderit dolore laboris incididunt pariatur eu ex est do velit officia excepteur . Et sicing aliquip culpa eiusmod.</p>
+                            <h2>Tail Waggin’ Turkey</h2>
+                            <p>Put some pep in their step with our high-protein turkey recipe. Made with lean protein, fresh veggies, and healthy grains, this meal is perfect for...</p>
+                            <Link className="buyButton" to="/">See More</Link>
                         </div>
                     </div>
-                </div><div className="card">
+                </div> */}
+                {/* <div className="card">
                     <div className="imgBox">
-                        <img src="/turkey/turkey_1-e66d7583287e48cc3cfc824652ccbd71.jpg" alt="turkey" />
+                        <img src="/turkey/turkey1.jpg" alt="turkey" />
                     </div>
                     <div className="content">
                         <div>
-                            <h2>Food Four</h2>
-                            <p>Reprehenderit dolore laboris incididunt pariatur eu ex est do velit officia excepteur . Et sicing aliquip culpa eiusmod.</p>
+                            <h2>Lip Lickin’ Lamb</h2>
+                            <p>Delicious, nutritious, and gentle as a lamb, this meal is perfect for pups that struggle with food sensitivities. Crafted with quinoa, an ancient grain...</p>
+                            <Link className="buyButton" to="/">See More</Link>
                         </div>
                     </div>
-                </div>
+                </div> */}
             </div>
         </section>
     );
-
-
 }
 
 export default Meals;
